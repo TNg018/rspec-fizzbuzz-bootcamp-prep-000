c41a458100e80392602eedb54c85ee7250947d13
Tien Nguyen <pri859@sudomail.com>
2018-05-30 16:46:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,7 @@
-fizz_3 = fizzbizz(3)
-puts "Fizz"
+describe "fizzbuzz" do
+  it 'returns "Fizz" when the number is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
+
+    expect(fizz_3).to eq("Fizz")
+  end
+end
