406146396d99f926e98ef4f6216d47d3f5a83461
Tien Nguyen <pri859@sudomail.com>
2018-05-30 17:19:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
-def fizzbuzz()
-
+def fizzbuzz(int)
+  if int % 3==0 && int % 5==0
+    "FizzBuzz"
+  elsif int % 3==0
+    "Fizz"
+  elsif int % 5==0
+    "Buzz"
+  else
+    nil
+  end
 end
