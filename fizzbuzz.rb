e5accc185f89f644626d0ebb8151a144d45afea7
Tien Nguyen <pri859@sudomail.com>
2018-05-30 17:06:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,8 @@
 def fizzbuzz(int)
-if int % 3 == 0
-return "Fizz"
+if int % 3 == 0 and int % 5 == 0
+  return "Fizzbuzz"
+elsif int % 3 == 0
+  return "Fizz"
+elsif int % 5 == 0
+  return "Buzz"
 end
